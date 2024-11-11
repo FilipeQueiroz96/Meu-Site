1249fa76957f326239978396c18abddfcd3637ad
FilipeQueiroz96 <filipequeiroz196@gmail.com>
2024-11-11 11:42:33 -0300

    mobilezando

--- script.js
+++ script.js
@@ -2,49 +2,39 @@
 window.addEventListener("scroll", function(){
     var menuheader = document.getElementById('header')
     var mouse = document.getElementById('mouse')
+    var menumobile = document.getElementById('botaomenu')
 
     menuheader.classList.toggle('scrollmenu',window.scrollY > 180)
     mouse.classList.toggle('scrollmouse', window.scrollY > 300)
+    menumobile.classList.toggle('scrollmenumobile', window.scrollY > 700)
 })
 
 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
+function clicou() {
+                 
+  botaomenu.style.display = 'none'
+   if (menumobile.style.width == '60%'){
+       menumobile.style.width = '0%'
+   } else{
+       menumobile.style.width = '60%'
+   }
+   }
 
-
-
-
-
-
-
-
-//     var swiper = new Swiper(".swiper", {
-//         loop: true,
-//         grabCursor: true,
-//         navigation: {
-//           nextEl: ".swiper-button-next",
-//           prevEl: ".swiper-button-prev",
-//         },
-//         pagination: {
-//             el: ".swiper-pagination",
-//             clickable: true,
-//           },    
-
-//           breakpoints: {
-//             // when window width is >= 320px
-//             400: {
-//               slidesPerView: 2,
-//               spaceBetween: 15
-//             },
-//             // when window width is >= 480px
-//             600: {
-//               slidesPerView: 2,
-//               spaceBetween: 10
-//             },
-//             // when window width is >= 640px
-//             1100: {
-//               slidesPerView: 2,
-//               spaceBetween: 5
-//             }
-//           }      
-
-//   });
+function fechar(){
+       menumobile.style.width = '0%'
+       botaomenu.style.display = 'block'
+   }
