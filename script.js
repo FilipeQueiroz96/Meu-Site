012794dbffd620ac48afc32233a1921b54a75c09
FilipeQueiroz96 <filipequeiroz196@gmail.com>
2024-11-19 09:13:14 -0300

    safari-fix

--- script.js
+++ script.js
@@ -4,38 +4,10 @@ window.addEventListener("scroll", function(){
     var mouse = document.getElementById('mouse')
     var menumobile = document.getElementById('botaomenu')
     
-    
-    // var home = document.getElementById('home')
-    // var sobre = document.getElementById('sobre')
-    // var servicos = document.getElementById('servicos')
-    // var portfolio = document.getElementById('portfolio')
-    // var depoimentos = document.getElementById('depoimentos')
 
     menuheader.classList.toggle('scrollmenu',window.scrollY > 180)
     mouse.classList.toggle('scrollmouse', window.scrollY > 300)
     menumobile.classList.toggle('scrollmenumobile', window.scrollY > 700)
-    
-    
-    
-    
-//      home.classList.remove('scrolls')
-//      sobre.classList.remove('scrolls')
-//      servicos.classList.remove('scrolls')
-//      portfolio.classList.remove('scrolls')
-//      depoimentos.classList.remove('scrolls')
-
-//     if(window.scrollY < 500 ){
-//       home.classList.add('scrolls')
-//     } else if(this.window.scrollY > 500 && window.scrollY < 1200 ){
-//       sobre.classList.add('scrolls')
-//     } else if(this.window.scrollY > 1200 && window.scrollY < 1900 ){
-//       servicos.classList.add('scrolls')
-//     } else if(this.window.scrollY > 1900 && window.scrollY < 2600 ){
-//       portfolio.classList.add('scrolls')
-//     } else if(this.window.scrollY > 2600 && window.scrollY < 2900 ){
-//       depoimentos.classList.add('scrolls')
-//     }
-// 
 })
 
 
@@ -50,6 +22,10 @@ var swiper = new Swiper(".mySwiper", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
   },
 });
 
